b7cd449f5c090223aa5541392698399e89964684
Oleg <rasskazov-2004@mail.ru>
2024-05-26 18:12:42 +0500

    Добавлена возможность загрузать изображения из папки

---
+++ Neuroinformatics_Laba4/Image.py
@@ -0,0 +1,37 @@
+﻿import sys
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from PIL import Image, ImageQt
+import matplotlib.pyplot as plt
+import numpy as np
+
+transform = transforms.Compose([transforms.ToTensor()])
+test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=4, shuffle=True)
+
+def convertToTensor(image):
+    scaled_image = image.scaled(32, 32)
+    pil_image = ImageQt.fromqimage(scaled_image)
+    transform = transforms.Compose([transforms.PILToTensor()])
+    img_tensor = transform(pil_image)
+    return img_tensor
+
+# Функция для сохранения изображений из DataLoader
+def save_images_from_loader(loader, save_folder):
+    for i, data in enumerate(loader):
+        images, labels = data
+        for j in range(len(images)):
+            image = transforms.ToPILImage()(images[j])
+            image.save(save_folder + 'image_' + str(i*len(images) + j) + '.png')
+            
+def imshow(img):
+    img = img / 2 + 0.5
+    npimg = img.numpy()
+    plt.figure(figsize=(10, 10))
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+save_folder = "C:\\Oleg\\Images\\CIFAR-10"
+#save_images_from_loader(test_loader, save_folder)
---
+++ Neuroinformatics_Laba4/RecognizeImages.py
@@ -0,0 +1,61 @@
+﻿import os
+import torch
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision import transforms
+import torchvision.transforms as transforms
+from PIL import Image
+from torch.autograd import Variable
+import torch.nn.functional as F
+import numpy as np
+
+"""transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])"""
+
+transform = transforms.Compose([
+    transforms.Resize((32, 32)),
+    transforms.ToTensor(),
+])
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 65)
+        self.fc3 = nn.Linear(65, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+#img = Image.open("C:\\Oleg\\Images\\CIFAR-10\\Bird1.png")
+folder_path = "C:\\Oleg\\Images\\CIFAR-10"
+
+model = Net()
+model.load_state_dict(torch.load('CNN.pth'))
+model.eval()
+
+cifar10_classes = ["Самолёт", "Автомобиль", "Птица", "Кошка", "Олень", "Собака", "Лягушка", "Лошадь", "Корабль", "Грузовик"]
+
+for filename in os.listdir(folder_path):
+    if filename.endswith('.png'):  # Проверка на формат изображения
+        # Загрузка изображения
+        img = Image.open(os.path.join(folder_path, filename))
+        img_tensor = transform(img).unsqueeze(0)
+        with torch.no_grad():
+            output = model(img_tensor)
+        _, predicted = torch.max(output, 1)
+        print('Класс от нейросети:', cifar10_classes[predicted.item()])
+
+
